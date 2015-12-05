b906558b3961f99ac36a65d6a569cf19d5efd107
nickhillwd <nickhillwd@gmail.com>
2015-12-05 15:06:05 +0000

    edit function pulling throughb id info running correctly

--- runner.rb
+++ runner.rb
@@ -26,13 +26,30 @@ post '/bookmarks' do
 end
 
 # get individual task from DB where id = :id
-get '/booomarks/:id' do
-  sql = "SELECT * FROM tasks WHERE id = #{params[:id]}"
+get '/bookmarks/:id' do
+  sql = "SELECT * FROM bookmarks WHERE id = #{params[:id]}"
   @individual_bookmark = run_sql(sql)
   erb :show
 end
 
 # retrieve and edit a task from DB where id = :id
+get '/bookmarks/:id/edit' do
+  sql = "SELECT * FROM bookmarks WHERE id = #{params[:id]}"
+  @edit_bookmark = run_sql(sql).first
+  erb :edit
+end
+
+post '/tasks/:id' do
+  # persists the edited task to the DB where id = :id
+  new_name = params[:name]
+  new_details = params[:details]
+  edit_id = params[:id]
+  sql = "UPDATE tasks SET name = '#{new_name}', details = '#{new_details}' WHERE id = #{edit_id}"
+
+  run_sql(sql)
+  redirect to("/tasks/#{params[:id]}")
+end
+
 # persists the edited task to the DB where id = :id
 # deleted task from DB where id = :id
 
